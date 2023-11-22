d68d33141cb4958be951ac97544dabd2cb78f365
Luanarm <luanarmadeira.lrm@gmail.com>
2023-11-22 10:20:12 -0500

    ellehacks2024

---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Hello extends Component {
+render(){
+    return (
+      <div>
+        <h1>Hello World!AAAAAAAAAAAAAAAa</h1>
+      </div>
+    );
+  }
+}  
+  export default Hello;
